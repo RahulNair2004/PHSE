f6c117f490975c8d632c038d0f9b35d555d8ea9d
Rahul Nair <rahul.nair0305@gmail.com>
2026-04-02 19:20:43 +0530

    Change DATABASE_URL to a placeholder
    Updated DATABASE_URL to a placeholder for connection string.

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 
-DATABASE_URL = "postgresql://postgres:rahul2004@localhost:5432/PHSE"
+DATABASE_URL = "yourownconnection"
 
 engine  = create_engine(DATABASE_URL)
 
